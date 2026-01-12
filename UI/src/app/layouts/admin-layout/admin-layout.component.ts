008835b0ccace5e09c5b786dbeb3ada9db621dac
monish <monish@wimerasys.com>
2026-01-12 11:21:58 +0530

    EchartAdding

--- UI/src/app/layouts/admin-layout/admin-layout.component.ts
+++ UI/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,32 +1,28 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { NgClass, NgFor, NgIf } from '@angular/common';
+import {  DatePipe, NgFor, NgIf } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [NgFor, NgIf, NgClass, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [NgFor, NgIf, RouterLink, RouterLinkActive, RouterOutlet,MatIconModule,DatePipe ],
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
-
+currentDateTime: Date = new Date();
   isCollapsed = true;
   isMobile = false;
   sidebarVisible = false;
 
   openSubmenuIndex: number | null = null;
   hoverMenuIndex: number | null = null;
-
+ sidebarOpen: boolean = false;
   menuItems = [
-    {
-      label: 'Dashboard',
-      icon: 'fas fa-home',
-      route: '/dashboard'
-    },
-    {
-      label: 'Settings',
-      icon: 'fas fa-cog',
+    { label: 'Dashboard', icon: 'dashboard', route: '/dashboard' },
+    { 
+      label: 'Settings', icon: 'settings',
       submenu: [
         { label: 'Devices', route: '/settings/device' },
         { label: 'User', route: '/settings/user' },
@@ -40,6 +36,9 @@ export class AdminLayoutComponent implements OnInit {
   ngOnInit() {
     this.checkScreenSize();
     this.expandActiveParent();
+    setInterval(() => {
+      this.currentDateTime = new Date();
+    }, 1000);
   }
 
   @HostListener('window:resize')
@@ -51,6 +50,7 @@ export class AdminLayoutComponent implements OnInit {
   }
 
   toggleSidebar() {
+     this.sidebarOpen = !this.sidebarOpen;
     if (this.isMobile) {
       this.sidebarVisible = !this.sidebarVisible;
     } else {
